99e3bd3622b892082a34164dbdfb82c34b022d45
Yx-Z <yx-z@outlook.com>
2025-06-28 12:13:52 -0400

    c54f62e04c52614932a651c3e7f87ebf4118032   match offset.direction:             case Direction.UP:                 return Coordinate(self.left, self.top - offset.offset)             case Direction.DOWN:                 return Coordinate(self.left, self.top + offset.offset)             case Direction.LEFT:                 return Coordinate(self.left - offset.offset, self.top)             case Direction.RIGHT:                 return Coordinate(self.left + offset.offset, self.top)

--- example/local_main.py
+++ example/local_main.py
@@ -2,11 +2,6 @@
 Local entry point.
 """
 
-import sys
-from pathlib import Path
-
-sys.path.append(str(Path(__file__).parents[1].resolve()))
-
 from interior_scene import interior_scene
 from nextrpg.game import Game
 
