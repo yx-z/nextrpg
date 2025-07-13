9fea4045e6117ddc1b0bea385d9dce113eb6476b
Yx-Z <yx-z@outlook.com>
2025-07-13 13:56:04 -0400

    fade in/out

--- nextrpg/draw/fade_out.py
+++ nextrpg/draw/fade_out.py
@@ -7,7 +7,7 @@ from nextrpg.draw.draw_on_screen import DrawOnScreen
 
 
 @dataclass(frozen=True)
-class FadeIn:
+class FadeOut:
     draw_on_screen: DrawOnScreen
     duration: Millisecond = field(
         default_factory=lambda: config().transition.duration
@@ -16,7 +16,8 @@ class FadeIn:
 
     def tick(self, time_delta: Millisecond) -> Self:
         if (elapsed := self._elapsed + time_delta) < self.duration:
-            alpha = alpha_from_percentage(elapsed / self.duration)
+            remaining = self.duration - elapsed
+            alpha = alpha_from_percentage(remaining / self.duration)
             draw_on_screen = self.draw_on_screen.set_alpha(alpha)
             return replace(self, draw_on_screen=draw_on_screen)
         return self
