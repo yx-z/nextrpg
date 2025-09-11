babae5f88a44c3de9515d2371a56562af97ece33
Yx-Z <yx-z@outlook.com>
2025-09-11 16:27:44 -0400

    clean

--- nextrpg/config/game_loop_config.py
+++ nextrpg/config/game_loop_config.py
@@ -5,5 +5,5 @@ from nextrpg.core.time import Millisecond
 
 @dataclass(frozen=True)
 class GameLoopConfig:
-    max_frames_per_second: int = 120
+    max_frames_per_second: int = 60
     garbage_collect_time_threshold: Millisecond | None = 10
