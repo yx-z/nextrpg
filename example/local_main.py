5ca9b1141a99eb9ab4d141ce2ff27368561a6ca6
yx-z <yx-z@outlook.com>
2025-10-07 08:16:09 -0400

    clean

--- example/local_main.py
+++ example/local_main.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
 
-sys.path.append(str((Path(__file__) / "../..").absolute()))
+sys.path.append(str((Path(__file__) / "../..").resolve()))
 from example.component.title import title
 from nextrpg import Config, DebugConfig, Game
 
