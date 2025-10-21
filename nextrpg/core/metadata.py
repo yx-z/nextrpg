80dc70df65625a2f5a9cc5cddbc984334ec5766f
Yx-Z <yx-z@outlook.com>
2025-10-21 18:33:55 -0400

    clean

--- nextrpg/core/metadata.py
+++ nextrpg/core/metadata.py
@@ -4,11 +4,16 @@ from typing import Any, Protocol, Self
 
 class HasMetadata(Protocol):
     def add_metadata(self, **kwargs: Any) -> Self:
+        from nextrpg.config.config import config
+
         assert is_dataclass(
             self
         ), f"Can only add metadata to dataclasses. Got {self}."
         assert isinstance(
             meta := getattr(self, "metadata", None), dict
         ), f"Need self.metadata dict. Got {self}."
-        metadata = meta | kwargs
-        return replace(self, metadata=metadata)
+
+        if (debug := config().debug) and debug.add_metadata:
+            metadata = meta | kwargs
+            return replace(self, metadata=metadata)
+        return self
