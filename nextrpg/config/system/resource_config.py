918d426bd60060f588c83ba23e8b5c1ddb61d725
Yx-Z <yx-z@outlook.com>
2025-11-19 08:31:44 -0500

    clean

--- nextrpg/config/system/resource_config.py
+++ nextrpg/config/system/resource_config.py
@@ -7,4 +7,4 @@ class ResourceConfig:
     map_scene_cache_size: int = 8
     sound_cache_size: int = 8
     save_slot_cache_size: int = 8
-    drawing_cache_size: int = 16
+    drawing_cache_size: int = 1024
